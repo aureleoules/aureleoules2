2d48384d0da443d66c606f1a16fca827e425996f
Aurèle Oulès <aurele@oules.com>
2017-04-07 03:34:40 +0200

    Google Analyctics; CNAME: webpack; changed favicon using own assets

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -1,26 +1,33 @@
 var webpack = require('webpack');
-var path    = require('path');
-var config  = require('./webpack.config');
+var path = require('path');
+var config = require('./webpack.config');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 config.output = {
-  filename: '[name].bundle.js',
-  publicPath: '',
-  path: path.resolve(__dirname, 'dist')
+	filename: '[name].bundle.js',
+	publicPath: '',
+	path: path.resolve(__dirname, 'dist')
 };
 
 config.plugins = config.plugins.concat([
+	new CopyWebpackPlugin([{
+		from: 'client/assets',
+		to: 'assets'
+	}, {
+    from: 'client/root/',
+    to: ''
+	}]),
+	// Reduces bundles total size
+	new webpack.optimize.UglifyJsPlugin({
+		mangle: {
 
-  // Reduces bundles total size
-  new webpack.optimize.UglifyJsPlugin({
-    mangle: {
-
-      // You can specify all variables that should not be mangled.
-      // For example if your vendor dependency doesn't use modules
-      // and relies on global variables. Most of angular modules relies on
-      // angular global variable, so we should keep it unchanged
-      except: ['$super', '$', 'exports', 'require', 'angular']
-    }
-  })
+			// You can specify all variables that should not be mangled.
+			// For example if your vendor dependency doesn't use modules
+			// and relies on global variables. Most of angular modules relies on
+			// angular global variable, so we should keep it unchanged
+			except: ['$super', '$', 'exports', 'require', 'angular']
+		}
+	})
 ]);
 
 module.exports = config;
