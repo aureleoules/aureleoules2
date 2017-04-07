72ce4f709a70e0e5c166fb79493165e7f47d31f7
Aurèle Oulès <aurele@oules.com>
2017-04-07 14:23:24 +0200

    change bg color on lclick home

--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -1,7 +1,13 @@
 class HomeController {
-  constructor() {
-    this.name = 'home';
-  }
+	constructor() {
+		this.name = 'home';
+		var hero = document.getElementById('hero');
+		var classes = ['is-info', 'is-success', 'is-warning', 'is-danger', 'is-light', 'is-dark', 'is-primary'];
+		hero.addEventListener('click', () => {
+			var random = Math.floor(Math.random() * classes.length);
+			hero.className = "hero is-fullheight " + classes[random];
+		});
+	}
 }
 
 export default HomeController;
